3df534aa810abde680bc263729077af34eef095f
Mohamed Khattab <Mohamed.e.khattab.0@gmail.com>
2023-07-09 12:31:19 +0300

    add queries

---
+++ Back-end/queries/queries.js
@@ -0,0 +1,8 @@
+const { sequelize, Sequelize } = require('../models/Board');
+const { sequelize, Sequelize } = require('../models/Elements');
+const { sequelize, Sequelize } = require('../models/Game');
+const { sequelize, Sequelize } = require('../models/User');
+const { sequelize, Sequelize } = require('../models/Usergame');
+
+
+const getCurrentCell=()+>
