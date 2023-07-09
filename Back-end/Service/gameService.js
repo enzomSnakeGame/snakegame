9ad31914b942388840aa92ab79b1046ddb606ad8
Mohamed Khattab <Mohamed.e.khattab.0@gmail.com>
2023-07-09 14:57:04 +0300

    add a move function

--- Back-end/Service/gameService.js
+++ Back-end/Service/gameService.js
@@ -1,8 +1,49 @@
-
-const getAllElements = require("../queries/queries");
-
-const move =(dice ) =>{
+const elementsService = require("../Repository/gameRepository");
 
 
+async function checkIfNewPositionExists(newPosition) {
+    try {
+      const elementsArray = await elementsService.getAllElements();
+      const newPositionExists = elementsArray.some(element => {
+        return element.start === newPosition;
+      });
+  
+      if (newPositionExists) {
+        console.log("New position exists");
+        // Do something if the new position exists
+      } else {
+        console.log("New position does not exist");
+        // Do something if the new position does not exist
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  }
 
-}
+  const move = async () => {
+    // Get random number between 1 and 6
+    const randomDice = Math.floor(Math.random() * 6) + 1;
+    try {
+      const elementsArray = await elementsService.getAllElements();
+  
+      const position = await elementsService.getPlayerPositionByRoomAndTurn();
+      let newPosition = position + randomDice;
+      
+      if (newPosition === size) {
+        console.log('Winner');
+      } else if (newPosition > size) {
+        console.log('Try again');
+      } else {
+        const matchingElement = elementsArray.find(element => element.start === newPosition);
+        if (matchingElement) {
+          newPosition = matchingElement.end;
+        }
+          // call a function to update 
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+  
+
+
