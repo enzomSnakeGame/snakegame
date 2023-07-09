ed54946f539584c535c160f755177ba83e849e0a
Mohamed Khattab <Mohamed.e.khattab.0@gmail.com>
2023-07-09 15:00:53 +0300

    update the function

--- Back-end/Service/gameService.js
+++ Back-end/Service/gameService.js
@@ -1,31 +1,13 @@
 const elementsService = require("../Repository/gameRepository");
 
 
-async function checkIfNewPositionExists(newPosition) {
-    try {
-      const elementsArray = await elementsService.getAllElements();
-      const newPositionExists = elementsArray.some(element => {
-        return element.start === newPosition;
-      });
-  
-      if (newPositionExists) {
-        console.log("New position exists");
-        // Do something if the new position exists
-      } else {
-        console.log("New position does not exist");
-        // Do something if the new position does not exist
-      }
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  }
+
 
   const move = async () => {
     // Get random number between 1 and 6
     const randomDice = Math.floor(Math.random() * 6) + 1;
     try {
       const elementsArray = await elementsService.getAllElements();
-  
       const position = await elementsService.getPlayerPositionByRoomAndTurn();
       let newPosition = position + randomDice;
       
