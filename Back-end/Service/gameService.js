1dee2290bcf72e1e7378fab6c2b8f21317cf7124
Habiba Osama <habebaosama14@gmail.com>
2023-07-10 12:05:09 +0300

    status

--- Back-end/Service/gameService.js
+++ Back-end/Service/gameService.js
@@ -2,10 +2,8 @@ const gameRepository = require("../Repository/gameRepository");
 
   const move = async (idRoom , turn) => {
     // Get random number between 1 and 6
-    //const randomDice = Math.floor(Math.random() * 6) + 1;
-    const randomDice =1 
+    const randomDice = Math.floor(Math.random() * 6) + 1;
 
-    console.log(randomDice) ;
     console.log("Random",randomDice);
     const size = 100 ; 
     try {
@@ -21,9 +19,11 @@ const gameRepository = require("../Repository/gameRepository");
       if (newPosition === size) {
         console.log('Winner');
         gameRepository.updateEndDate();
+        gameRepository.updatePlayerStatus(idRoom, turn);
         return -1;
       } else if (newPosition > size) {
         console.log('Try again');
+        gameRepository.updatePlayerStatus(idRoom, turn);
         return position;
       } else {
         const matchingElement = elementsArray.find(element => element.start === newPosition);
@@ -32,12 +32,17 @@ const gameRepository = require("../Repository/gameRepository");
           newPosition = matchingElement.end;
           console.log("d5lt",newPosition)
         }
-        gameRepository.updatePlayerPosition(newPosition);
+      const position = await gameRepository.getPlayerPositionByRoomAndTurn(idRoom , turn);
+        gameRepository.updatePlayerPosition(newPosition,idRoom,turn);
+        gameRepository.updatePlayerStatus(idRoom, turn);
         return newPosition;
     }
+    
+
     } catch (error) {
       console.error('Error:', error);
     }
+
   };
 
   module.exports = {
