8d1cf3c54996adeec89a2040aa591ca9095b06a4
abdelaziz <90736727+Abdelaziz25@users.noreply.github.com>
2023-07-11 15:35:10 +0300

    edit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,18 +7,27 @@ import CardPage from './pages/Card';
 
 function App() {
   return (
-    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div>
+
+<BrowserRouter>
+        <Routes>
+        <Route path="/board" element={<BoardPage/>} />
+        </Routes>
+      </BrowserRouter> 
+         <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div className="max-w-md w-full space-y-8">
      <BrowserRouter>
         <Routes>
             <Route path="/" element={<LoginPage/>} />
             <Route path="/signup" element={<SignupPage/>} />
-            <Route path="/board" element={<BoardPage/>} />
+           
             <Route path="Card/" element={<CardPage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
   </div>
+    </div>
+   
 
   );
 }
