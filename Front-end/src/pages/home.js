460a0aefa8f8bde0a97c04b9dc03153d51e910bc
Mohamed Khattab <Mohamed.e.khattab.0@gmail.com>
2023-07-11 14:48:30 +0300

    Revert "add home component"
    This reverts commit 38f4cea808572185f95b83d5e5309a6698298243.

--- Front-end/src/pages/home.js
+++
@@ -1,9 +0,0 @@
-import HomeComponent from  "../components/HomeComponent.js"
-
-export default function HomePage(){
-    return(
-        <>
-            <HomeComponent/>
-        </>
-    )
-}
--- front-end/src/components/BoardComponent.js
+++ front-end/src/components/BoardComponent.js
@@ -1,12 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState,useEffect  } from 'react';
 
-const Container = ({ children }) => {
-  return (
-    <div style={{ width: '100%' }}>
-      {children}
-    </div>
-  );
-};
 
 const colors = ['teal', 'blue', 'green', 'yellow', 'orange', 'purple', 'pink', 'cyan', 'magenta', 'lime'];
 
@@ -27,7 +20,6 @@ const generatePlayerTokens = (numTokens) => {
   }
   return playerTokens;
 };
-
 function App() {
   const [diceNumber, setDiceNumber] = useState(null);
   let numTokens = 10;
@@ -44,57 +36,69 @@ function App() {
     }
   }, [countdown]);
 
+
   const rollDice = () => {
     const randomNumber = Math.floor(Math.random() * 6) + 1;
     setDiceNumber(randomNumber);
   };
-
+  
+ 
   return (
-    <Container>
-      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <h2 style={{ marginLeft: '240px', marginBottom: '10px' }}>Snakes and Ladders</h2>
-          <div style={{ position: 'relative', width: '100%' }}>
+    <view>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
+        <h2 style={{ marginLeft: '240px', marginBottom: '10px' }}>Snakes and Ladders</h2>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+        
+          <div style={{ textAlign: 'center', marginRight: '20px' }}>
+            <p style={{ fontWeight: 'bold', marginBottom: '5px' }}>Player Turn</p>
+            <p style={{ fontSize: '24px', margin: '0' }}>1</p>
+          </div>
+          <div style={{ position: 'relative' }}>
             <img
               src={require('../snake ladder.png')}
               alt="logo"
               style={{ width: '100%' }}
             />
-            {playerTokens.map((token, index) => (
-              <div
-                key={index}
-                style={{
-                  position: 'absolute',
-                  top: '500px',
-                  left: `${token.left}px`,
-                  width: '40px',
-                  height: '40px',
-                  borderRadius: '50%',
-                  backgroundColor: token.color,
-                }}
-              />
-            ))}
+           
+             {playerTokens.map((token, index) => (
+            <div
+              key={index}
+              style={{
+                position: 'absolute',
+                top: '500px',
+                left: `${token.left}px`,
+                width: '40px',
+                height: '40px',
+                borderRadius: '50%',
+                backgroundColor: token.color,
+              }}
+            />
+          ))}
           </div>
           <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-            <button
-              style={{ margin: '20px', backgroundColor: 'lightblue', width: '100px', height: '40px', borderRadius: '20px', fontWeight: 'bold', fontSize: '15px' }}
-              onClick={rollDice}
-            >
-              Roll Dice
-            </button>
-            {diceNumber && (
-              <p style={{ fontSize: '24px', margin: '0' }}>{diceNumber}</p>
-            )}
-            <div style={{ textAlign: 'center', marginTop: '30px' }}>
-              <p style={{ fontWeight: 'bold', marginBottom: '5px', marginLeft: '10px' }}>Countdown</p>
-              <p style={{ fontSize: '24px', margin: '0' }}>{countdown}</p>
-            </div>
-          </div>
+          
+  <button
+    style={{ margin: '20px', backgroundColor: 'lightblue', width: '100px', height: '40px', borderRadius: '20px', fontWeight: 'bold', fontSize: '15px' }}
+    onClick={rollDice}
+  >
+    Roll Dice
+  </button>
+  {diceNumber && (
+    <p style={{ fontSize: '24px', margin: '0' }}>{diceNumber}</p>
+  )}
+
+<div style={{ textAlign: 'center', marginTop: '30px' }}>
+          <p style={{ fontWeight: 'bold', marginBottom: '5px',marginLeft:'10px' }}>Countdown</p>
+          <p style={{ fontSize: '24px', margin: '0' }}>{countdown}</p>
         </div>
+</div>
+        </div>
+      
       </div>
-    </Container>
+    </div>
+    </view>
   );
 }
-
 
 export default App;
