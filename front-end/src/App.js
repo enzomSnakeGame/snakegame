e5a4e87be166607e084a37e68f68a902647203b6
MahmoudElsh3rawy <mahmoudelsharawy92@gmail.com>
2023-07-12 23:02:45 +0300

    merge main

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -18,18 +18,7 @@ function App() {
   sessionStorage.setItem('token',data);
   return (
     <div>
-<<<<<<< HEAD
-    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
-     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<LoginPage/>} />
-            <Route path="/signup" element={<SignupPage/>} />
-            <Route path="/board" element={<BoardPage/>} />
-            <Route path="Card/" element={<CardPage/>}/>
-        </Routes>
-      </BrowserRouter>
-=======
+
    
 <BrowserRouter>
         <Routes>
@@ -54,13 +43,13 @@ function App() {
               
           </Routes>
         </BrowserRouter>
->>>>>>> main
+
     </div>
   </div>
     </div>
   </div>
   );
-}
+} 
 
 export {socket};
 export default App;
