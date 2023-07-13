566bfe8e9fe7d4dbc4f775b9a2740dfee9b364f6
Mohamed Khattab <Mohamed.e.khattab.0@gmail.com>
2023-07-13 16:33:45 +0300

    socket change

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -22,11 +22,11 @@ function App() {
     <div>
       <Routes> 
         <Route path="/" element={<LoginPage/>} />
-        <Route path="/home" element={<HomePage/>}/>
+        <Route path="/home" element={<HomePage  socket = {socket}/>}/>
       <Route path="/board" element={<BoardPage socket = {socket}/>} />
         <Route path="/pending" element={<PendingPage socket = {socket}/>}/>
         <Route path="/loginPage" element={<LoginPage/>} />
-        <Route path="/signup" element={<SignupPage socket = {socket}/>} />
+        <Route path="/signup" element={<SignupPage/>} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="pageNotFound" element={<PageNotFound />} />
       </Routes>
