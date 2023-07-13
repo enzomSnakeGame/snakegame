72a1a8acea16d179e8d10b1d64e47ab153629dbe
Mohamed Khattab <Mohamed.e.khattab.0@gmail.com>
2023-07-13 15:06:21 +0300

    add  a white background for the login /signup

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -5,7 +5,7 @@ export default function LoginPage(){
     return(
         <>
         <div>
-            <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8" style={{ backgroundColor: 'white' }}>
                 <div className="max-w-md w-full space-y-8">
                     <Header
                         heading="Login to your account"
--- front-end/src/pages/Signup.js
+++ front-end/src/pages/Signup.js
@@ -5,7 +5,7 @@ export default function SignupPage(){
     return(
         <>
         <div>
-            <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+            <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8" style={{ backgroundColor: 'white' }}>
                 <div className="max-w-md w-full space-y-8">
                     <Header
                         heading="Signup to create an account"
